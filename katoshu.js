16e27b0b31eacf5ed223f6d5cc53debabfd884d1
freshFH2017 <shuriskishunsuke@gmail.com>
2018-05-06 22:42:04 +0900

    changed

--- katoshu.js
+++ katoshu.js
@@ -1,14 +1,3 @@
-// sed -e 's/ジ/ディ/g'
-// sed -e 's/チ/ティ/g'
-// sed -e 's/ヂ/ディ/g'
-// sed -e 's/ツ/トゥ/g'
-// sed -e 's/ヅ/ドゥ/g'
-// sed -e 's/バ/ヴァ/g'
-// sed -e 's/ビ/ヴィ/g'
-// sed -e 's/ブ/ヴ/g'
-// sed -e 's/ベ/ヴェ/g'
-// sed -e 's/ボ/ヴォ/g'
-
 var henkan = function(input){
   var before = [/ジ(?!(ャ|ュ|ョ))/g, /チ(?!(ャ|ュ|ョ))/g, /ヂ/g, /ツ/g, /ヅ/g, /バ/g, /ビ(?!(ャ|ュ|ョ))/g, /ブ/g,/ベ/g, /ボ/g];
   var after  = ["ディ","ティ","ディ","トゥ","ドゥ","ヴァ","ヴィ","ヴ", "ヴェ","ヴォ"];
@@ -19,8 +8,7 @@ var henkan = function(input){
   return input;
 };
 
-function getValue(idname){
+var getValue = function(idname){
   var result = document.getElementById(idname).value;
-  str = henkan(result);
-  alert(str);
+  document.getElementById("str").innerText=henkan(result);
 }
