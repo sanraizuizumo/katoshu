8024efe662e1afc9c76d2559fcd786cee272143a
freshFH2017 <shuriskishunsuke@gmail.com>
2018-05-06 19:52:46 +0900

    fixed ja ju jo...

--- katoshu.js
+++ katoshu.js
@@ -10,8 +10,8 @@
 // sed -e 's/ボ/ヴォ/g'
 
 var henkan = function(input){
-  var before = [/ジ/g,  /チ/g,  /ヂ/g,  /ツ/g,  /ヅ/g,  /バ/g,  /ビ/g,  /ブ/g,/ベ/g,  /ボ/g];
-  var after  = ["ディ","ティ","ディ","トゥ","ドゥ","ヴァ","ヴィ","ヴ","ヴェ","ヴォ"];
+  var before = [/ジ(?!(ャ|ュ|ョ))/g, /チ(?!(ャ|ュ|ョ))/g, /ヂ/g, /ツ/g, /ヅ/g, /バ/g, /ビ(?!(ャ|ュ|ョ))/g, /ブ/g,/ベ/g, /ボ/g];
+  var after  = ["ディ","ティ","ディ","トゥ","ドゥ","ヴァ","ヴィ","ヴ", "ヴェ","ヴォ"];
 
   for(var i=0, len=before.length; i<len; i++){
     input = input.replace(before[i], after[i],"g");
